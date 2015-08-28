c2fca84b7b2a21b49dd950729b1ddd401b24addc
Pradeep <spradeepv@gmail.com>
2015-08-28 15:21:14 +0530

    VMware Logical Hub Controller Solution

--- hackerrank/vmware/logical_hub_controller.py
+++ hackerrank/vmware/logical_hub_controller.py
@@ -8,7 +8,10 @@ def get_input():
         l = inp.split()
         hub_ids = l[1:]
         hosts_dict.update({l[0]: hub_ids})
-        inp = raw_input()
+        try:
+            inp = raw_input()
+        except EOFError:
+            break
 
 def get_mappings():
     hosts.extend(hosts_dict.keys())
@@ -25,8 +28,8 @@ def get_mappings():
                     l.append(host)
                 else:
                     hub_id_dict.update({hub_id: [host]})
-    print hubs
-    print hub_id_dict
+    #print hubs
+    #print hub_id_dict
 
 def port_to_port(host, indices):
     length = len(indices)
@@ -36,15 +39,40 @@ def port_to_port(host, indices):
             if i != j:
                 print s, i, j
 
+def port_to_tunnel(src_host, src_port, dest_host, dest_port):
+    if dest_port:
+        s = "PORT_TO_TUNNEL"
+        if len(dest_port) > 1:
+            l = []
+            for port in dest_port:
+                if port not in l:
+                    print s, src_host, src_port, dest_host, port
+                    l.append(port)
+        else:
+            print s, src_host, src_port, dest_host, dest_port[0]
+
+def tunnel_to_port(host, hub, port):
+    s = "TUNNEL_TO_PORT"
+    print s, host, hub, port
+
 def get_command():
     for host in hosts:
         hubs = hosts_dict.get(host)
-        l =
-        for hub in hubs:
+        for hub in list(set(hubs)):
             indices = [i for i, x in enumerate(hubs) if x == hub]
             if len(indices) > 1:
-                print indices
                 port_to_port(host, indices)
+        index = 0
+        for src_hub in hubs:
+            dest_hosts = hub_id_dict.get(src_hub)
+            for dest_host in dest_hosts:
+                if host != dest_host:
+                    dest_hubs = hosts_dict.get(dest_host)
+                    indices = [x for i, x in enumerate(dest_hubs) if x == src_hub]
+                    port_to_tunnel(host, index, dest_host, indices)
+            if len(dest_hosts) > 1:
+                tunnel_to_port(host, src_hub, index)
+            index += 1
 
 get_input()
 get_mappings()
