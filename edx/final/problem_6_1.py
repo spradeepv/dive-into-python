209653d3c022e6ed5d6e24c31136485c6a0fa8cc
Pradeep Sathasivam <psathasi@brocade.com>
2015-10-27 19:27:49 +0530

    EDX - Final Solution

--- edx/final/problem_6_1.py
+++ edx/final/problem_6_1.py
@@ -29,26 +29,53 @@ def insert(atMe, newFrob):
     """
     print atMe.name, newFrob.name
     if atMe.name < newFrob.name:
-        newFrob.before = atMe
         print "IF"
+        newFrob.before = atMe
         temp = atMe
         found = False
-        while temp.after and newFrob.name < temp.after.name:
+        while temp.after and newFrob.name > temp.after.name:
             temp = temp.after
-            print "WHILE", temp.name
             found = True
-        temp.after = newFrob
+        print temp.name
         if found:
+            newFrob.before = temp
+            newFrob.after  = temp.after
+            if temp.after:
+                temp.after.before = newFrob
+            temp.after = newFrob
+        else:
+            newFrob.before = atMe
+            newFrob.after = atMe.after
+            if atMe.after:
+                atMe.after.before = newFrob
             atMe.after = newFrob
-            newFrob.after = temp
-
+        # if found:
+        #     atMe.after = newFrob
+        #     temp.before = newFrob
+        #     newFrob.after = temp
+        # else:
+        #     temp.after = newFrob
     else:
-        print "ELSE"
+        print "IF"
         newFrob.after = atMe
-        while atMe.before and newFrob.name > atMe.before.name:
-            atMe = atMe.before
-            print "WHILE"
-        atMe.before = newFrob
+        temp = atMe
+        found = False
+        while temp.before and newFrob.name < temp.before.name:
+            temp = temp.before
+            found = True
+        print temp.name
+        if found:
+            newFrob.after = temp
+            newFrob.before = temp.before
+            if temp.before:
+                temp.before.after = newFrob
+            temp.before = newFrob
+        else:
+            newFrob.after = atMe
+            newFrob.before = atMe.before
+            if atMe.before:
+                atMe.before.after = newFrob
+            atMe.before = newFrob
 
 eric = Frob('eric')
 andrew = Frob('andrew')
@@ -64,10 +91,37 @@ print "*******************************"
 print andrew.before, andrew.after.name
 print eric.before.name, eric.after.name
 print fred.before.name, fred.after.name
-print martha.before, martha.after.name
-print ruth.before.name, ruth.after.name
+print martha.before.name, martha.after.name
+print ruth.before.name, ruth.after
+print "*******************************"
 
+allison = Frob('allison')
+lyla = Frob("lyla")
+christina =Frob("christina")
+ben = Frob("ben")
+#insert(allison, lyla)
+#insert(allison, christina)
+#insert(allison, ben)
+#print "*******************************"
+#print allison.before, allison.after.name
+#print ben.before.name, ben.after.name
+#print christina.before.name, christina.after.name
+#print lyla.before.name, lyla.after
+#print "*******************************"
 
+lenoid = Frob('leonid')
+a = Frob('amara')
+j1 = Frob('jennifer')
+j2 = Frob('jennifer')
+s = Frob('scott')
+
+insert(lenoid, s)
+insert(s, j1)
+print "*******************************"
+print j1.before, j1.after.name
+print lenoid.before.name, lenoid.after.name
+print s.before.name, s.after
+print "*******************************"
 
 
 
