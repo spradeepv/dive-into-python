f2a540407fccf3c68539f6b829556d7f63946827
spradeepv <spradeepv@gmail.com>
2016-01-14 17:18:25 +0530

    Visa

---
+++ hackerrank/visa/ip-address-validation.py
@@ -0,0 +1,33 @@
+import re
+
+for _ in range(int(raw_input())):
+    s = raw_input()
+    l = s.split(".")
+    if len(l) == 4:
+        is_valid = True
+        for c in l:
+            addr = int(c)
+            if addr < 0 or addr > 255:
+                is_valid = False
+                break
+        if is_valid:
+            print "IPV4"
+            continue
+    else:
+        ipv6 = s.split(":")
+        pattern = r"^[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]$"
+        is_valid = True
+        if len(ipv6) == 8:
+            for addr in ipv6:
+                if len(addr) < 4:
+                    for i in range(4 - len(addr)):
+                        addr = "0"+addr
+                if re.match(pattern, addr):
+                    continue
+                else:
+                    is_valid = False
+                    break
+            if is_valid:
+                print "IPV6"
+                continue
+    print "Neither"
